8e91e1ddb5e13c770bffeecaa7b5858bfd6c59d7
Reynaldo Gil <gilcu2@gmail.com>
2024-09-04 06:35:06 +0200

    Brew formula update for topdiffxml version v0.4.8

--- topdiffxml.rb
+++ topdiffxml.rb
@@ -5,20 +5,20 @@
 class Topdiffxml < Formula
   desc "Generate top differences between 2 xml documents."
   homepage "https://github.com/gilcu2/topdiffxml"
-  version "0.4.3"
+  version "0.4.8"
   depends_on :macos
 
   on_intel do
-    url "https://github.com/gilcu2/topdiffxml/releases/download/v0.4.3/topdiffxml_Darwin_x86_64.tar.gz"
-    sha256 "05bbf657f4ce59379c24f1be70306e921bc8a44cef1e20e85c75eed6d7475d8a"
+    url "https://github.com/gilcu2/topdiffxml/releases/download/v0.4.8/topdiffxml_Darwin_x86_64.tar.gz"
+    sha256 "b234b1cbcc3a7157b5c822400d240ac02592a000a49b3760f00a33bb0daaaab9"
 
     def install
       bin.install "topdiffxml"
     end
   end
   on_arm do
-    url "https://github.com/gilcu2/topdiffxml/releases/download/v0.4.3/topdiffxml_Darwin_arm64.tar.gz"
-    sha256 "ee38e96863536a7e8582eb0dfe89243e0845d04af56353e40e545ac276959b72"
+    url "https://github.com/gilcu2/topdiffxml/releases/download/v0.4.8/topdiffxml_Darwin_arm64.tar.gz"
+    sha256 "33aa0880589b23a5fd865d7de89d64c7c474900f9526b0ab9a13fc7ae2714b54"
 
     def install
       bin.install "topdiffxml"
